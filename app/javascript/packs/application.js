5988b1b35e7cd0578b1e69f456ba82b7cb690899
alcoholisment <alcoholism1025@gmail.com>
2020-09-14 17:10:12 +0900

    _

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//= require jquery3
+//= require popper
+//= require bootstrap
+
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+//= require jquery
+//= require jquery_ujs
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -19,3 +25,9 @@ import 'bootstrap'
 import '../src/application.scss'
 import '@fortawesome/fontawesome-free/js/all'
 
+
+$(function() {
+  $('.a').slick({
+      dots: true,
+  });
+});
